9dadc803195c6eefa86dd9ace1623c8b16f90765
zxcvb <xuejingwei_1129@outlook.com>
2022-03-14 20:48:49 +0800

    [post页面作者]

--- templates/content/content-single.php
+++ templates/content/content-single.php
@@ -18,6 +18,18 @@
                         <div class="post-tag">
                             <i class="fa fa-tags"></i><?php the_tags("<ul><li>", "</li><span>/</span><li>", "</li></ul>") ?>
                         </div>
+                        <div class="post-author">
+                            <?php $author = get_the_author_meta("ID"); ?>
+                            <div class="author-avatar">
+                                <?php echo get_avatar($author) ?>
+                            </div>
+                            <div class="author-profile">
+                                <div>
+                                    <span>作者：</span><?php the_author_posts_link() ?>
+                                </div>
+                                <div><?php echo get_the_author_meta("description"); ?></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="block-corner-deco">
                         <span class="tl">❊</span>
