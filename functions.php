0dda4703c3f93c3cf605a0d0314f67d523e42f1c
zxcvb <xuejingwei_1129@outlook.com>
2022-03-23 20:11:31 +0800

    [针对windows平台的特定滚动条样式]+[some fix]

--- functions.php
+++ functions.php
@@ -6,7 +6,32 @@ function wp_load_css_js()
     wp_enqueue_style("font-awesome", get_template_directory_uri() . "/assets/includes/font-awesome-4.7.0/css/font-awesome.min.css", null, null, "screen");
     wp_enqueue_script("site-function", get_template_directory_uri() . "/assets/js/bundle.js", [], null, true);
 }
+//add platform check
+function platform_check()
+{
+?>
+    <script>
+        const platform = function() {
+            var os = navigator.oscpu,
+                pl1 = navigator.platform,
+                pl2 = navigator.userAgentData && navigator.userAgentData.platform;
+            if (os && os.includes("Windows") || pl1 && pl1.includes("Win32") || pl2 && pl2.includes("Windows")) {
+                return "Win";
+            }
+            if (os && os.includes("Mac OS") || pl1 && pl1.includes("MacIntel") || pl2 && pl2.includes("macOS")) {
+                return "Mac";
+            }
+        }();
+        if (platform === "Win") {
+            var style = document.createElement("style");
+            style.innerHTML = `::-webkit-scrollbar{width:6px;background:#f7f1e5}::-webkit-scrollbar-thumb{background:#888;border-radius:6px}:root{scrollbar-width:thin;scrollbar-color:#888 #f7f1e5}`;
+            document.head.appendChild(style);
+        }
+    </script>
+<?php
+}
 add_action("wp_enqueue_scripts", "wp_load_css_js");
+add_action("wp_head", "platform_check");
 //remove unused default actions from wp_head hook
 remove_action("wp_head", "wp_resource_hints", 2);
 remove_action("wp_head", "feed_links", 2);
